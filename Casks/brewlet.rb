cb20f21e2117d430599a98a102238f2671f0078f
Zaid Kokaja <zkokaja@users.noreply.github.com>
2020-12-18 15:29:03 -0500

    Update to universal version 1.5

--- Casks/brewlet.rb
+++ Casks/brewlet.rb
@@ -1,7 +1,7 @@
 cask 'brewlet' do
 
-  version '1.5'
-  sha256 '110101e5804a86b57f83a967b9fa38c0c9a46a4463a8a1a1d6017c5d5a4a0477'
+  version '1.5-universal'
+  sha256 'f9786a7f31502b2cc7dac8013822e96da5ecbee38a0518a4ed8eee9081bb8cf9'
 
   url "https://github.com/zkokaja/Brewlet/releases/download/v#{version}/Brewlet.zip"
   appcast 'https://github.com/zkokaja/Brewlet/releases.atom'
